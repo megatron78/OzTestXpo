4ee6d2bce353ffa51be09ff5d0a65bc9f4159a6b
megatron <mauri_molina75@hotmail.com>
2017-08-04 14:02:29 -0500

    Barra de búsqueda preescolar

---
+++ resources/views/vendor/adminlte/layouts/partials/searchpre.blade.php
@@ -0,0 +1,143 @@
+<div class="row">
+    <div class="box box-info">
+        <form style="background-color: #2C2D2D;" class="form-inline">
+            <div class="box-body">
+                {{--<span style="font-size: 18px;color: ghostwhite;" class="glyphicon glyphicon glyphicon-search"></span>
+                &nbsp;&nbsp;&nbsp;--}}
+                <div class="form-group">
+                    <label style="font-size: 14px; color: ghostwhite;" for="search_province">Provincia: &nbsp;</label>
+                    <select style="width: 120px" name="search_province" id="search_province" class="form-control">
+                        <option value="">...</option>
+                        @foreach($provinces as $province)
+                            <option value="{{ $province->code}}">{{ $province->name}}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="form-group">
+                    &nbsp;&nbsp;
+                    <label style="font-size: 14px; color: ghostwhite;" for="search_canton">Cantón: &nbsp;</label>
+                    <select id="search_canton" class="form-control">
+                        <option selected="selected">Alabama</option>
+                        <option>Alaska</option>
+                        <option>California</option>
+                        <option>Delaware</option>
+                        <option>Tennessee</option>
+                        <option>Texas</option>
+                        <option>Washington</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    &nbsp;&nbsp;
+                    <label style="font-size: 14px; color: ghostwhite;" for="search_parish">Parroquia: &nbsp;</label>
+                    <select id="search_parish" class="form-control">
+                        <option selected="selected">Alabama</option>
+                        <option>Alaska</option>
+                        <option>California</option>
+                        <option>Delaware</option>
+                        <option>Tennessee</option>
+                        <option>Texas</option>
+                        <option>Washington</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    &nbsp;&nbsp;
+                    <label style="font-size: 14px; color: ghostwhite;" for="search_city">Ciudad: &nbsp;</label>
+                    <select id="search_city" class="form-control">
+                        <option selected="selected">Alabama</option>
+                        <option>Alaska</option>
+                        <option>California</option>
+                        <option>Delaware</option>
+                        <option>Tennessee</option>
+                        <option>Texas</option>
+                        <option>Washington</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    &nbsp;&nbsp;
+                    <label style="font-size: 14px; color: ghostwhite;" for="search_institution" class="control-label">Nombre: &nbsp;</label>
+                    <input type="text" id="search_institution" class="form-control mx-sm-3"
+                           placeholder="Institución...">
+                </div>
+                <button type="submit" style="width: 120px;" class="btn btn-warning">BUSCAR</button>
+                <a style="font-size: 14px" data-toggle="collapse" data-parent="#accordion"
+                   href="#advancedSearch">&nbsp;&nbsp;
+                    <strong> Avanzada <i class="fa fa-search-plus margin-r-5"></i></strong>
+                </a>
+            </div>
+            <div id="advancedSearch" class="box-body panel-collapse collapse">
+                <div class="form-group">
+                    &nbsp;&nbsp;<label style="font-size: 14px; color: ghostwhite;" class="form-check-label">
+                        <input type="checkbox" class="form-check-input" id="advsearch_chkFiscal"
+                               value="public">
+                        Fiscal
+                    </label>
+                </div>&nbsp;&nbsp;
+                <div class="form-group">
+                    &nbsp;&nbsp;<label style="font-size: 14px; color: ghostwhite;" class="form-check-label">
+                        <input type="checkbox" class="form-check-input" id="advsearch_chkFiscomisional"
+                               value="public_private">
+                        Fiscomisional
+                    </label>
+                </div>&nbsp;&nbsp;
+                <div class="form-group">
+                    &nbsp;&nbsp;<label style="font-size: 14px; color: ghostwhite;" class="form-check-label">
+                        <input type="checkbox" class="form-check-input" id="advsearch_chkParticular"
+                               value="private">
+                        Particular
+                    </label>
+                </div>&nbsp;&nbsp;
+                <div style="border-left:1px solid whitesmoke;" class="form-group">
+                    &nbsp;&nbsp;<label style="font-size: 14px; color: ghostwhite;" class="form-check-label">
+                        <input type="checkbox" class="form-check-input" id="advsearch_chkLaico"
+                               value="laic">
+                        Laico
+                    </label>
+                </div>&nbsp;&nbsp;
+                <div class="form-group">
+                    &nbsp;&nbsp;<label style="font-size: 14px; color: ghostwhite;" class="form-check-label">
+                        <input type="checkbox" class="form-check-input" id="advsearch_chkReligious"
+                               value="religious">
+                        Religioso
+                    </label>
+                </div>&nbsp;&nbsp;
+                <div style="border-left:1px solid whitesmoke;" class="form-group">
+                    &nbsp;&nbsp;<label style="font-size: 14px; color: ghostwhite;" class="form-check-label">
+                        <input type="checkbox" class="form-check-input" id="advsearch_chkFemale"
+                               value="all_female">
+                        Mujeres
+                    </label>
+                </div>&nbsp;&nbsp;
+                <div class="form-group">
+                    &nbsp;&nbsp;<label style="font-size: 14px; color: ghostwhite;" class="form-check-label">
+                        <input type="checkbox" class="form-check-input" id="advsearch_chkMaleFemale"
+                               value="male_female">
+                        Mixto
+                    </label>
+                </div>&nbsp;&nbsp;
+                <div class="form-group">
+                    &nbsp;&nbsp;<label style="font-size: 14px; color: ghostwhite;" class="form-check-label">
+                        <input type="checkbox" class="form-check-input" id="advsearch_chkMale"
+                               value="all_male">
+                        Hombres
+                    </label>
+                </div>&nbsp;&nbsp;
+                <div style="border-left:1px solid whitesmoke;" class="form-group">
+                    &nbsp;&nbsp;<label style="font-size: 14px; color: ghostwhite;" class="form-check-label">
+                        <input type="checkbox" class="form-check-input" id="advsearch_chkExtended"
+                               value="extended_schedule">
+                        Horario Extendido
+                    </label>
+                </div>&nbsp;&nbsp;
+                <div style="border-left:1px solid whitesmoke;" class="form-group">
+                    &nbsp;&nbsp;&nbsp;<label style="font-size: 14px; color: ghostwhite;"  for="advsearch_costo">Costo: <b>$ 0</b></label>
+                    &nbsp;&nbsp;&nbsp;&nbsp;
+                    <input type="text" value="" class="slider form-control"
+                           data-slider-min="0" data-slider-max="500" data-slider-step="50"
+                           data-slider-value="[0,150]" data-slider-orientation="horizontal"
+                           data-slider-selection="before" data-slider-tooltip="show"
+                           data-slider-id="blue_price_slider"/>&nbsp;<b style="font-size: 14px; color: ghostwhite;" >$ 500+</b>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
