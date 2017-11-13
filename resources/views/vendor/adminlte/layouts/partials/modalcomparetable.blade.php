d403b797ea9b7eb12e73ec2bfe4d4ada79d708d3
megatron <mauri_molina75@hotmail.com>
2017-11-13 14:23:34 -0500

    Changes to UI acorde a revision enerprime

--- resources/views/vendor/adminlte/layouts/partials/modalcomparetable.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/modalcomparetable.blade.php
@@ -2,7 +2,7 @@
 <div class="modal fad" id="compareTableModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
-            <div class="modal-header">
+            <div style="background-color: #FF9302" class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span></button>
                 <h4 class="modal-title"><strong>Tabla Comparativa</strong></h4>
